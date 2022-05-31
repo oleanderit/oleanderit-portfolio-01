ff26da14eed9bdd8b80168a1dddec5e071ff4a77
mr-samsul-alam <contactsamsulalam@gmail.com>
2022-05-31 14:40:48 +0600

    no change

--- main-site/src/Pages/HomePage/NavigationBar/NavigationBar.js
+++ main-site/src/Pages/HomePage/NavigationBar/NavigationBar.js
@@ -1,53 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css'
 const NavigationBar = () => {
 
-    // const [show, setShow] = useState(false);
-    // const showDropdown = (e) => {
-    //     setShow(!show);
-    // }
-    // const hideDropdown = e => {
-    //     setShow(false);
-    // }
+    const [show, setShow] = useState(false);
+    const showDropdown = (e) => {
+        setShow(!show);
+    }
+    const hideDropdown = e => {
+        setShow(false);
+    }
     return (
-        <div class='container'>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to="/">Navbar</Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/home">Home</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/services">Services</Link>
-                            </li>
-
-
-                            <ul class="Services" aria-labelledby="navbarDropdownMenuLink">
-                                <li> <Link class="nav-link active" aria-current="page" to="/home">Home</Link></li> 
-                            </ul>
-
-                            
-                            <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/about">About</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/myOi">My OI</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/contactUs">Contact Us</Link>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <> <Container>
+            <div className='topNav'> <div style={{ display: 'flex', flexDirection: 'row', }}><p className='px-1 my-auto'>Email: info.oleanderit@gmail.com</p> <p className='px-1 my-auto'>Mobile:+123456789</p></div> <div><p className='px-1 my-auto'>Facebook</p></div></div>
+        </Container>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">Oleander IT</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center' }} to="/home">Home</Link>
+                            <NavDropdown title="Services" id="basic-nav-dropdown"
+                                show={show}
+                                onMouseEnter={showDropdown}
+                                onMouseLeave={hideDropdown}>
+                                <NavDropdown.Item href="#action/3.1">Web Development</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.2">Web Design(UI/UX)</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.3">Tracking System</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.3">Graphic Design</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.3">Social Marketing</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.3">MS Excel Solution</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.3">Virtual Card Solution</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center', marginRight: "10px" }} to="/about">About</Link>
+                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center', marginRight: "10px" }} to="/myOi">My OI</Link>
+                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center', marginRight: "10px" }} to="/contactUs">Contact Us</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
