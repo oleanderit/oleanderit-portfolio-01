55900e3cc2373b93734ad2c5ac29cac89fe7eb9c
coders-sohan <coders.sohan.0216@gmail.com>
2022-06-02 00:32:28 +0600

    update

--- main-site/src/Pages/HomePage/ProgressSec/ProgressSec.js
+++ main-site/src/Pages/HomePage/ProgressSec/ProgressSec.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const ProgressSec = () => {
-  const percentage1 = 66;
+	const percentageOfServices = [
+		{
+			name: "team support",
+			value: 40,
+		},
+		{
+			name: "project complete",
+			value: 75,
+		},
+		{
+			name: "success rate",
+			value: 80,
+		},
+		{
+			name: "good review",
+			value: 95,
+		},
+	];
 
 	return (
 		<>
@@ -16,9 +33,43 @@ const ProgressSec = () => {
 								productive business solutions
 							</h2>
 						</div>
-            <div>
-            <CircularProgressbar value={percentage1} text={`${percentage1}%`} />;
-            </div>
+						<div>
+							<div className="row align-items-center">
+								{percentageOfServices.map((percentage, index) => (
+									<div
+										className="col col-md-2 col-lg-3 px-4 py-5"
+										key={index + percentage.name}
+									>
+										<div className="h-50 w-50">
+											<CircularProgressbar
+												value={percentage.value}
+												text={`${percentage.value}%`}
+												styles={buildStyles({
+											
+													// Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+													strokeLinecap: 'butt',
+											
+													// Text size
+													textSize: '16px',
+											
+													// How long animation takes to go from one percentage to another, in seconds
+													pathTransitionDuration: 0.5,
+											
+													// Can specify path transition in more detail, or remove it entirely
+													// pathTransition: 'none',
+											
+													// Colors
+													pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+													textColor: '#f88',
+													trailColor: '#d6d6d6',
+													backgroundColor: '#3e98c7',
+												})}
+											/>
+										</div>
+									</div>
+								))}
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
