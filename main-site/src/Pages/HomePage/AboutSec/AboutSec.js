28a2816904a270709fe9b478d963449ceac942dd
coders-sohan <coders.sohan.0216@gmail.com>
2022-06-02 01:43:22 +0600

    progress remove

---
+++ main-site/src/Pages/HomePage/AboutSec/AboutSec.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./AboutSec.css";
+
+const AboutSec = () => {
+	return (
+		<>
+			<div>
+				<div>
+					<div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default AboutSec;
