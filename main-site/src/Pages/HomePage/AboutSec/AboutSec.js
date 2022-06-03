b386e4c98c53a0633e60d061eca35322f042b5f4
coders-sohan <coders.sohan.0216@gmail.com>
2022-06-03 20:02:02 +0600

    home page almost done

--- main-site/src/Pages/HomePage/AboutSec/AboutSec.js
+++ main-site/src/Pages/HomePage/AboutSec/AboutSec.js
@@ -1,13 +1,44 @@
 import React from "react";
+import chart from "../../../asset/chart.svg";
 import "./AboutSec.css";
 
 const AboutSec = () => {
 	return (
 		<>
-			<div>
-				<div>
-					<div>
-						<div></div>
+			<div className="about-sec bg-lightgray">
+				<div className="container mx-auto py-5">
+					<div className="d-flex align-items-center justify-content-between gap-5 py-3 flex-lg-row flex-column mx-auto">
+						<div>
+							<div className="pb-5 mx-auto">
+								<div>
+									<p className="text-uppercase m-0 text-gray">About Us</p>
+									<h1 className="fw-bold mt-2">
+										Best <span className="text-blue">Digital Agency</span> in
+										the Town
+									</h1>
+									<h5 className="d-flex my-4 heading-line">
+										<span>Science 2021</span>
+									</h5>
+									<p className="m-0 mb-4 ">
+										Pleasure rationally encounter consequences that are
+										extremely painful. Nor again is there anyone who loves or
+										pursues or desires to obtain
+									</p>
+									<q className="m-0 fw-bold">
+										Pleasure rationally encounter consequences that are
+										extremely painful. Nor again is there
+									</q>
+									<div className="mt-4">
+										<a href="/" className="btn btn-blue rounded-pill">
+											Learn more
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="text-center">
+							<img src={chart} alt="chart" className="w-80" />
+						</div>
 					</div>
 				</div>
 			</div>
--- main-site/src/Pages/HomePage/HomePage.js
+++ main-site/src/Pages/HomePage/HomePage.js
@@ -1,15 +1,22 @@
 import React from "react";
+import AboutSec from "./AboutSec/AboutSec";
+import BlogSec from "./BlogSec/BlogSec";
 import HeroSec from "./HeroSec/HeroSec";
 import "./HomePage.css";
 import NavigationBar from "./NavigationBar/NavigationBar";
+import NewsLetter from "./NewsLetter/NewsLetter";
+import ServiceSec from "./ServiceSec/ServiceSec";
 
 const HomePage = () => {
 	return (
 		<>
 			<NavigationBar />
 			<HeroSec />
-			{/* <ServiceSec />
-			<ProgressSec /> */}
+			<AboutSec />
+			<ServiceSec />
+			<BlogSec />
+			<NewsLetter />
+			{/* <ProgressSec /> */}
 		</>
 	);
 };
