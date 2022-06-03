f08c1fc8e82c6b111437d0b0d7d749880095e032
mr-samsul-alam <contactsamsulalam@gmail.com>
2022-06-04 04:11:41 +0600

    all

--- main-site/src/Pages/HomePage/NavigationBar/NavigationBar.js
+++ main-site/src/Pages/HomePage/NavigationBar/NavigationBar.js
@@ -21,31 +21,11 @@ const NavigationBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center' }} to="">Home</Link>
-                            <NavDropdown title="Services" id="basic-nav-dropdown"
-                                show={show}
-                                onClick={showDropdown}
-                                onBlur={hideDropdown}
-                                >
-                                <NavDropdown.Item href="#action/3.1">Web Development</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.2">Web Design(UI/UX)</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.3">Tracking System</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.3">Graphic Design</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.3">Social Marketing</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.3">MS Excel Solution</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.3">Virtual Card Solution</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center', marginRight: "10px" }} to="/about">About</Link>
-                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center', marginRight: "10px" }} to="/myOi">My OI</Link>
-                            <Link style={{ textDecoration: "none", display: 'flex', alignItems: 'center', marginRight: "10px" }} to="/contactUs">Contact Us</Link>
+                            <Link to="/" className='navBtn px-4 p-1' >Home</Link>
+                            <Link to="/services" className='navBtn px-4 p-1' >Services</Link>
+                            <Link to="/about" className='navBtn px-4 p-1'>About</Link>
+                            <Link to="/myOi" className='navBtn px-4 p-1'>My OI</Link>
+                            <Link style={{ backgroundColor: "#0b61b3", color: 'white', }} className="px-3 p-2 navBtn" to="/contactUs">LET'S TALK</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
